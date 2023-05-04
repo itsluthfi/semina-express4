ba9bbabb0807c485dca698ead200468b46cf745c
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2023-05-04 10:59:55 +0700

    feat: create custom errors

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ const categoriesRouter = require('./app/api/v1/categories/router');
 
 const v1 = '/api/v1';
 
+// middlewares
+const notFoundMiddleware = require('./app/middlewares/not-found');
+const handleErrorMiddleware = require('./app/middlewares/handler-error');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -23,5 +27,9 @@ app.get('/', (req, res) => {
 });
 
 app.use(`${v1}/cms`, categoriesRouter);
+
+// middlewares
+app.use(notFoundMiddleware);
+app.use(handleErrorMiddleware);
 
 module.exports = app;
