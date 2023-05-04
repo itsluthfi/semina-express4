b8f1c55454c36085421115f350575e03f8dc9bba
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2023-05-04 10:30:03 +0700

    feat: config app.js for routing

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-
 const app = express();
+
+// router
+
+const v1 = '/api/v1';
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -14,7 +15,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'Welcome to Semina API',
+  });
+});
 
 module.exports = app;
