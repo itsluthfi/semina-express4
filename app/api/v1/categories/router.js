fda73c6e4cf6ce651373f6c5121bbd95d036a495
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2023-05-23 17:23:25 +0700

    feat(categories): add authorization middleware in categories router

--- app/api/v1/categories/router.js
+++ app/api/v1/categories/router.js
@@ -6,10 +6,30 @@ const {
   authorizeRoles,
 } = require('../../../middlewares/auth');
 
-router.get('/categories', authenticateUser, index);
-router.get('/categories/:id', authenticateUser, find);
-router.put('/categories/:id', authenticateUser, update);
-router.delete('/categories/:id', authenticateUser, destroy);
-router.post('/categories', authenticateUser, create);
+router.get('/categories', authenticateUser, authorizeRoles('organizer'), index);
+router.get(
+  '/categories/:id',
+  authenticateUser,
+  authorizeRoles('organizer'),
+  find
+);
+router.put(
+  '/categories/:id',
+  authenticateUser,
+  authorizeRoles('organizer'),
+  update
+);
+router.delete(
+  '/categories/:id',
+  authenticateUser,
+  authorizeRoles('organizer'),
+  destroy
+);
+router.post(
+  '/categories',
+  authenticateUser,
+  authorizeRoles('organizer'),
+  create
+);
 
 module.exports = router;
