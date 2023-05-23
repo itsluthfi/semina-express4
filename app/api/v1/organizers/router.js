db40395beecb16a1b56e71db4e8fd5c5dade4d14
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2023-05-23 17:20:58 +0700

    feat(organizers): add auth middleware in organizers router

--- app/api/v1/organizers/router.js
+++ app/api/v1/organizers/router.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express();
 const { createCMSOrganizer, createCMSUser } = require('./controller');
 
-// const {
-//   authenticateUser,
-//   // authorizeRoles,
-// } = require('../../../middlewares/auth');
+const {
+  authenticateUser,
+  authorizeRoles,
+} = require('../../../middlewares/auth');
 
 router.post('/organizers', createCMSOrganizer);
-router.post('/users', createCMSUser);
+router.post('/users', authenticateUser, createCMSUser);
 
 module.exports = router;
