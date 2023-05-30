f7e7b292c7812c58b70fea6b191bcff3351489cb
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2023-05-30 19:12:52 +0700

    feat(events): add change status events function to service and controller

--- app/api/v1/events/controller.js
+++ app/api/v1/events/controller.js
@@ -69,10 +69,23 @@ const destroy = async (req, res, next) => {
   }
 };
 
+const changeStatus = async (req, res, next) => {
+  try {
+    const result = await changeStatusEvents(req);
+
+    res.status(StatusCodes.OK).json({
+      data: result,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
 module.exports = {
   index,
   find,
   update,
   destroy,
   create,
+  changeStatus,
 };
